a516f46fe2c7e78319175ec2066c48c502f9a86a
Nash <129913613+nashdevcode@users.noreply.github.com>
2025-05-22 17:37:56 -0500

    Initial Commit

---
+++ ZeroApp/ZeroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeroAppApp.swift
+//  ZeroApp
+//
+//  Created by kevin arica ramos on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ZeroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

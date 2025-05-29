eb86d9a02339ce8a389ee91d69612445ba921ae1
Victor Martinez <104571495+Mardaks@users.noreply.github.com>
2025-05-29 00:23:27 -0500

    Agregue el model Reservation y la vista CreateReservation, tambien modifique el ContentView, faltaria el DetailsReservation y modificar la interfaz

---
+++ ZeroApp/Model/Reservation.swift
@@ -0,0 +1,8 @@
+//
+//  Reservation.swift
+//  ZeroApp
+//
+//  Created by Victor Martinez on 28/05/25.
+//
+
+import Foundation
---
+++ ZeroApp/View/CreateReservationView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateReservationView.swift
+//  ZeroApp
+//
+//  Created by Victor Martinez on 28/05/25.
+//
+
+import SwiftUI
+
+struct CreateReservationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateReservationView()
+}
